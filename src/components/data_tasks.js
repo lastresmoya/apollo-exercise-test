2b1347cdcec4b274059a4df4199b0850cd0ab228
Rob Moya <lastresmoyallc@gmail.com>
2020-08-24 22:20:43 -0300

    WIP Apollo Exercise

---
+++ src/components/data_tasks.js
@@ -0,0 +1,3 @@
+const tasks = [{id: 1, taskDescription: 'Start React Development'}, {id: 2, taskDescription: 'Install Dependencias'}, {id: 3, taskDescription: 'Run Yarn or Npm Start'}, {id: 4, taskDescription: 'Create your Components'}, {id: 5, taskDescription: 'Delete Them'}]
+
+export {tasks};
